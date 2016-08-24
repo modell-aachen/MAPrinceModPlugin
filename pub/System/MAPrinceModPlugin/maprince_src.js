b68df10b19ae5bd9b4e8bd38fe840944ba45e288
Stephan Osthold <osthold@modell-aachen.de>
2016-08-24 09:09:04 +0200

    Rewrite to prevent exploiting file:// links
    No longer using GenPDFPrincePlugin since we changed about every line of
    code here.
    
    Will use a custom rest-handler for prince to download images and such.
    ACL checks will be done via a secret cookie.
    A JavaScript will change the src locations.
    
    Access to local files is no longer permitted.
    
    Changed the settings in configure, to make it much easier to install the
    plugin.
    
    Ticket: #13593
    Release-Note(fix, admin) {
      Improved security for PDF generation.
    }
    Release-Note(change, admin) {
      Simplified configuration of PDF generator.
    }

--- pub/System/MAPrinceModPlugin/maprince_src.js
+++ pub/System/MAPrinceModPlugin/maprince_src.js
@@ -1,6 +1,10 @@
 jQuery(function() {
     var $ = jQuery;
 
+    // allow pagebreaks in tables
+    //    * iserted by CKEditorPlugin
+    //    * will put a 'page-break-after' on the corresponding tr/td
+
     $('div>span:contains("PAGEBREAK")').each(function() {
         var $this = $(this);
 
@@ -14,6 +18,40 @@ jQuery(function() {
 
         //$allowBreak.each(function(){$(this).css('page-break-inside', 'auto');});
         $allowBreak.css('page-break-inside', 'auto');
+    });
+
+    // rewrite 'src' attributes to rest handler
+
+    // location of rest-handler
+    var $mapTo = $('script.MAPrinceModPluginMapTo');
+    if($mapTo.length != 1) return; // do not allow more, possably an exploit
+    var mapTo = $mapTo.text();
+
+    // things we want to rewrite
+    var regexes = [];
+    $('script.MAPrinceModPluginMapFrom').each(function() {
+        regexes.push(new RegExp('^' + $(this).text()));
+    });
+
+    // things we do not want to rewrite
+    var exceptions = [];
+    $('script.MAPrinceModPluginMapException').each(function() {
+        exceptions.push(new RegExp($(this).text()));
+    });
+
+    // do the rewrite
+    $('[src]').each(function() {
+        var $this = $(this);
+        var src = $this.attr('src');
+        $.each(regexes, function(idx, regex) {
+            if(regex.test(src)) {
+                var newSrc = src.replace(regex, '');
+                for(var i = 0; i < exceptions.length; i++) {
+                    if(exceptions[i].test(newSrc)) return;
+                }
+                $this.attr('src', mapTo + newSrc);
+            }
+        });
     });
 });
 
